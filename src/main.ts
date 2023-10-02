684bf2629a3caa1962f7a046ba57ea1a5e1e5a44
David Costa <davidcostadev@gmail.com>
2023-10-02 08:17:34 -0300

    change the host

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,36 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
 
-import { createContext } from './context';
+import { Context, createContext } from './context';
 import { schema } from './schema';
 
-const apolloServer = new ApolloServer({
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer<Context>({
   schema,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-startStandaloneServer(apolloServer, {
-  context: createContext,
-}).then(({ url }) => {
-  console.log('GraphQL API ready at', url);
-});
+async function main() {
+  await server.start();
+
+  app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    bodyParser.json({ limit: '50mb' }),
+    expressMiddleware(server, {
+      context: createContext,
+    }),
+  );
+
+  await new Promise<void>((resolve) => httpServer.listen(4000, '0.0.0.0', resolve));
+  console.log('🚀 Server ready at http://localhost:4000/');
+}
+
+main();
